0f3f967b409fa92bb1e26a669840e5b0dc656b5c
Alexander Hans <ahans@users.noreply.github.com>
2023-04-11 11:31:51 +0200

    speed it up

--- cpp/dbscan.cpp
+++ cpp/dbscan.cpp
@@ -2,6 +2,9 @@
 
 #include <nanoflann.hpp>
 
+#include <numeric>
+#include <iostream>
+
 namespace dbscan {
 
 namespace {
@@ -50,31 +53,115 @@ Dbscan::Dbscan(float const eps, std::uint32_t const min_samples, std::size_t con
 
 auto Dbscan::fit_predict(std::vector<Dbscan::Point> const& points) -> std::vector<Dbscan::Label>
 {
-    PointsVectorAdaptor adapter{points};
+    // PointsVectorAdaptor adapter{points};
 
-    constexpr auto num_dims{2};
-    constexpr auto leaf_size{32};
-    nanoflann::
-        KDTreeSingleIndexAdaptor<nanoflann::L2_Adaptor<float, PointsVectorAdaptor>, PointsVectorAdaptor, num_dims>
-            points_kd_tree{num_dims, adapter, nanoflann::KDTreeSingleIndexAdaptorParams{leaf_size}};
-    points_kd_tree.buildIndex();
+    // constexpr auto num_dims{2};
+    // constexpr auto leaf_size{32};
+    // nanoflann::
+    //     KDTreeSingleIndexAdaptor<nanoflann::L2_Adaptor<float, PointsVectorAdaptor>, PointsVectorAdaptor, num_dims>
+    //         points_kd_tree{num_dims, adapter, nanoflann::KDTreeSingleIndexAdaptorParams{leaf_size}};
+    // points_kd_tree.buildIndex();
 
-    nanoflann::SearchParams params{};
-    params.sorted = false;
+    // nanoflann::SearchParams params{};
+    // params.sorted = false;
 
     labels_.assign(std::size(points), undefined);
+
+    if (std::size(points) <= 1) {
+        return labels_;
+    }
 
     Label cluster_count{0};
     std::vector<Label> clusters{};
 
-    for (auto i{0UL}; i < std::size(points); ++i) {
+    // reorg point cloud
+
+    auto eps = std::sqrt(eps_squared_);
+
+    Dbscan::Point min{points[0]};
+    Dbscan::Point max{points[0]};
+    for (auto const& pt : points) {
+        min[0] = std::min(min[0], pt[0]);
+        min[1] = std::min(min[1], pt[1]);
+        max[0] = std::max(max[0], pt[0]);
+        max[1] = std::max(max[1], pt[1]);
+    }
+    // std::cout << "range: " << min[0] << ", " << min[1] << " --> " << max[0] << ", " << max[1] << std::endl;
+
+    float const range_x = max[0] - min[0];
+    float const range_y = max[1] - min[1];
+    auto const num_bins_x = static_cast<std::uint32_t>(std::ceil(range_x / eps));
+    auto const num_bins_y = static_cast<std::uint32_t>(std::ceil(range_y / eps));
+
+    std::vector<std::uint32_t> counts(num_bins_x * num_bins_y);
+
+    for (auto const& pt : points) {
+        auto const bin_x = static_cast<std::uint32_t>(std::floor((pt[0] - min[0]) / eps));
+        auto const bin_y = static_cast<std::uint32_t>(std::floor((pt[1] - min[1]) / eps));
+        auto const index = bin_y * num_bins_x + bin_x;
+        counts.at(index) += 1;
+    }
+    std::vector<std::uint32_t> offsets{};
+    offsets.reserve(std::size(counts));
+    std::exclusive_scan(std::cbegin(counts), std::cend(counts), std::back_inserter(offsets), 0);
+
+    auto scratch = offsets;
+    std::vector<Point> new_points(std::size(points));
+    std::vector<std::uint32_t> new_point_to_point_index_map(std::size(points));
+    std::uint32_t i{0};
+    for (auto const& pt : points) {
+        auto const bin_x = static_cast<std::uint32_t>(std::floor((pt[0] - min[0]) / eps));
+        auto const bin_y = static_cast<std::uint32_t>(std::floor((pt[1] - min[1]) / eps));
+        auto const index = bin_y * num_bins_x + bin_x;
+        auto new_pt_index = scratch.at(index)++;
+        new_points.at(new_pt_index) = pt;
+        new_point_to_point_index_map.at(new_pt_index) = i++;
+    }
+
+    std::vector<std::uint32_t> neighbors;
+    neighbors.reserve(16364);
+
+    auto square = [](auto const v) {
+        return v * v;
+    };
+
+    auto radius_search = [&](std::uint32_t pt_index) -> std::uint32_t {
+        neighbors.clear();
+        auto const bin_y = pt_index / num_bins_x;
+        auto const bin_x = pt_index % num_bins_x;
+        auto const pt = new_points.at(pt_index);
+        for (auto dy = -1; dy <= 1; ++dy) {
+            for (auto dx = -1; dx <= 1; ++dx) {
+                auto const neighbor_bin_y = bin_y + dy;
+                auto const neighbor_bin_x = bin_x + dx;
+                if (neighbor_bin_x < 0 || neighbor_bin_x >= num_bins_x || neighbor_bin_y < 0 ||
+                    neighbor_bin_y >= num_bins_y) {
+                    continue;
+                }
+                auto const neighbor_bin = neighbor_bin_y * num_bins_x + neighbor_bin_x;
+                for (auto i{0U}; i < counts.at(neighbor_bin); ++i) {
+                    auto const neighbor_pt_index = offsets.at(neighbor_bin) + i;
+                    if (neighbor_pt_index == pt_index) {
+                        continue;
+                    }
+                    auto const neighbor_pt = new_points.at(neighbor_pt_index);
+                    if ((square(neighbor_pt[0] - pt[0]) + square(neighbor_pt[1] - pt[1])) < eps_squared_) {
+                        neighbors.push_back(neighbor_pt_index);
+                    }
+                }
+            }
+        }
+        return neighbors.size();
+    };
+
+    for (auto i{0UL}; i < std::size(new_points); ++i) {
         // skip point if it has already been processed
         if (labels_[i] != undefined) {
             continue;
         }
 
         // find number of neighbors of current point
-        if (points_kd_tree.radiusSearch(points[i].data(), eps_squared_, neighbors_, params) < min_samples_) {
+        if (radius_search(i) < min_samples_) {
             labels_[i] = noise;
             continue;
         }
@@ -86,13 +173,13 @@ auto Dbscan::fit_predict(std::vector<Dbscan::Point> const& points) -> std::vecto
         labels_[i] = current_cluster_id;
 
         to_visit_.clear();
-        visited_.assign(std::size(points), false);
+        visited_.assign(std::size(new_points), false);
 
-        for (auto const& n : neighbors_) {
-            if (!visited_[n.first]) {
-                to_visit_.push_back(n.first);
+        for (auto const& n : neighbors) {
+            if (!visited_[n]) {
+                to_visit_.push_back(n);
             }
-            visited_[n.first] = true;
+            visited_[n] = true;
         }
 
         for (auto j{0UL}; j < std::size(to_visit_); ++j) {
@@ -115,19 +202,24 @@ auto Dbscan::fit_predict(std::vector<Dbscan::Point> const& points) -> std::vecto
             labels_[neighbor] = current_cluster_id;
 
             // and query its neighborhood to see if it also to be considered as a core point
-            if (points_kd_tree.radiusSearch(points[neighbor].data(), eps_squared_, neighbors_, params) < min_samples_) {
+            if (radius_search(neighbor) < min_samples_) {
                 continue;
             }
-            for (auto const& n : neighbors_) {
-                if (!visited_[n.first]) {
-                    to_visit_.push_back(n.first);
+            for (auto const& n : neighbors) {
+                if (!visited_[n]) {
+                    to_visit_.push_back(n);
                 }
-                visited_[n.first] = true;
+                visited_[n] = true;
             }
         }
     }
 
-    return labels_;
+    std::vector<Label> labels(std::size(labels_));
+    for (auto i{0U}; i < std::size(labels_); ++i) {
+        labels.at(new_point_to_point_index_map.at(i)) = labels_.at(i);
+    }
+
+    return labels;
 }
 
 }  // namespace dbscan
