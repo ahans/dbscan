1ae6cbd222787d24753fb24b3ea9539d59cf53ff
Alexander Hans <ahans@users.noreply.github.com>
2023-07-17 11:52:11 +0200

    Add blobs test and extend benchmark

--- python/dbscan_test.py
+++ python/dbscan_test.py
@@ -9,7 +9,7 @@ from cpp import py_dbscan
 
 def test_moon():
     """Basic test using two moons sample."""
-    X, y = datasets.make_moons(n_samples=1000)
+    X, _ = datasets.make_moons(n_samples=1000)
     dbscan = py_dbscan.DBSCAN(0.05, 10)
     y_pred = dbscan.fit_predict(X)
 
@@ -24,6 +24,43 @@ def test_moon():
     assert y_pred.shape[0] == y_pred_sklearn.shape[0]
     num_matching = np.count_nonzero(y_pred == y_pred_sklearn)
     assert num_matching == pytest.approx(1000, abs=10)
+
+
+def test_blobs():
+    """Test using 20 2-D blobs samples."""
+    centers = (
+        (16, 0),
+        (2, 2),
+        (10, 2),
+        (15, 3),
+        (1, 5),
+        (18, 5),
+        (7, 6),
+        (13, 7),
+        (3, 9),
+        (15, 10),
+        (9, 11),
+        (1, 13),
+        (5, 14),
+        (18, 14),
+        (10, 15),
+        (15, 15),
+        (2, 18),
+        (6, 19),
+        (17, 19),
+        (11, 22),
+    )
+    n_blobs = len(centers)
+    X, _ = datasets.make_blobs(
+        n_samples=10_000, centers=centers, cluster_std=0.1, random_state=42
+    )
+
+    dbscan = py_dbscan.DBSCAN(0.5, 20)
+    y_pred = dbscan.fit_predict(X)
+    num_classes = np.array(
+        [np.count_nonzero(y_pred == label) for label in range(n_blobs)]
+    )
+    assert np.all(num_classes == 500)
 
 
 pytest.main([__file__, "-rP"])
