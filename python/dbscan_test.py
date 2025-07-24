fix: change num_bins calculation instead of bins index directly

python/dbscan_test.py
@@ -61,8 +61,8 @@ def test_blobs():
     assert all(np.count_nonzero(y_pred == label) == 500 for label in range(n_blobs))
 
 
-def test_two_points():
-    """Test using two points."""
+def test_points_on_border():
+    """Test using two points on the border with a eps which divides point / min_point without remainder."""
     X = np.array([[0, 0], [1, 1]])
 
     dbscan = py_dbscan.DBSCAN(0.5, 2)